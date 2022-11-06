fc7f2c16b9c76b0254c138ed26a7212c6649f187
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-07 00:27:02 +0900

    import modules and create and display github auth

---
+++ src/components/Auth.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { signIn } from "next-auth/react";
+
+const Auth: React.FC = () => {
+  return (
+    <div>
+      <button
+        className="rounded bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800"
+        onClick={() => signIn("github")}
+      >
+        Github Auth
+      </button>
+    </div>
+  );
+};
+
+export default Auth;
