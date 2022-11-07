855bfe054d0467cfd1b09e303044b7bfd0e215e6
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-07 13:05:41 +0900

    create useStore

---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import create from "zustand/react";
+import { UpdateTaskInput } from "../schema/todo";
+
+type State = {
+  editedTask: UpdateTaskInput;
+  updateEditedTask: (payload: UpdateTaskInput) => void;
+  resetEditedTask: () => void;
+};
+
+const useStore = create<State>((set) => ({
+  editedTask: { taskId: "", title: "", body: "" },
+  updateEditedTask: (payload) =>
+    set({
+      editedTask: payload,
+    }),
+  resetEditedTask: () =>
+    set({
+      editedTask: { taskId: "", title: "", body: "" },
+    }),
+}));
+
+export default useStore;
