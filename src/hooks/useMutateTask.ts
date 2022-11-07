0f79ae9b0469b036f7c08fdc3059021ca4c4ab16
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-07 13:26:46 +0900

    create mutations

--- src/hooks/useMutateTask.ts
+++ src/hooks/useMutateTask.ts
@@ -2,5 +2,40 @@ import { trpc } from "../utils/trpc";
 import useStore from "../store";
 
 export const useMutateTask = () => {
-  return {};
+  const utils = trpc.useContext();
+  const reset = useStore((state) => state.resetEditedTask);
+
+  const createTaskMutation = trpc.todo.createTask.useMutation({
+    onSuccess: (res) => {
+      const previousTodos = utils.todo.getTasks.getData();
+      if (previousTodos) {
+        utils.todo.getTasks.setData([res, ...previousTodos]);
+      }
+      reset();
+    },
+  });
+
+  const updateTaskMutation = trpc.todo.updateTask.useMutation({
+    onSuccess: (res) => {
+      const previousTodos = utils.todo.getTasks.getData();
+      if (previousTodos) {
+        utils.todo.getTasks.setData(
+          previousTodos.map((task) => (res.id === task.id ? res : task))
+        );
+      }
+    },
+  });
+
+  const deleteTaskMutation = trpc.todo.deleteTask.useMutation({
+    onSuccess: (_, variables) => {
+      const previousTodos = utils.todo.getTasks.getData();
+      if (previousTodos) {
+        utils.todo.getTasks.setData(
+          previousTodos.filter((task) => variables.taskId !== task.id)
+        );
+      }
+    },
+  });
+
+  return { createTaskMutation, updateTaskMutation, deleteTaskMutation };
 };
