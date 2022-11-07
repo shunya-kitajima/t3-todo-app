817ed69b5005d0c9307cd9593aabc6d43ef18b62
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-07 10:59:48 +0900

    create updateTaskSchema and UpdateTaskInput

--- src/schema/todo.ts
+++ src/schema/todo.ts
@@ -6,3 +6,15 @@ export const createTaskSchema = z.object({
 });
 
 export type CreateTaskInput = z.TypeOf<typeof createTaskSchema>;
+
+export const updateTaskSchema = z.object({
+  taskId: z.string().cuid(),
+  title: z.string().max(20),
+  body: z.string().min(5),
+});
+
+export type UpdateTaskInput = z.TypeOf<typeof updateTaskSchema>;
+
+export const getSingleTaskSchema = z.object({
+  taskId: z.string().cuid(),
+});
