15b32a7abca19e50362d7eef59f21b7ead9eae78
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-07 13:12:49 +0900

    add case of zoderror

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -5,8 +5,17 @@ import { ZodError } from "zod";
 
 export const t = initTRPC.context<Context>().create({
   transformer: superjson,
-  errorFormatter({ shape }) {
-    return shape;
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
+            ? error.cause.flatten()
+            : null,
+      },
+    };
   },
 });
 
